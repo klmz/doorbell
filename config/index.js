704299312567b0c8de89b586d5433de9db5b7ced
rknotter <68162118+rknotter@users.noreply.github.com>
2020-08-29 21:40:46 +0200

    Update config file

--- config/index.js
+++ config/index.js
@@ -1,8 +1,11 @@
 var config = module.exports = {};
 
-config.camType = 'ONVIF'; // ONVIF | USB
+// Configuration for the camera type 
+config.camType = 'ONVIF'; // ONVIF | USB | NONE
 
-config.xaddrOnvif = 'http://192.168.1.4:9001/onvif/devices';
-config.userOnvif = 'Wokkels';
-config.passOnvif = 'Neuteltje123!';
+config.xaddrOnvif = 'http://192.168.1.4:9001/onvif/devices'; //Replace axample with ONVIF address
+config.userOnvif = 'username';
+config.passOnvif = 'password';
 
+// Configuration for sensorservice
+config.sensorEnabled = false; // TRUE | FALSE
